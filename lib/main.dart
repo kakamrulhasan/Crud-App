6d5c2baf4a18ced4258786122f009df5f4df3ba3
kamrul Hasan <kakamrul2000@gmail.com>
2024-05-25 03:19:24 +0600

    crud_app_adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_36/product_list_screen.dart';
+import 'package:flutter_application_36/crud_app.dart';
 
 void main() {
   runApp( MainApp());
-}
-
-class MainApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        inputDecorationTheme: InputDecorationTheme(
-          
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.orange)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.orange)
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red)
-                  ),
-                  focusedErrorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red)
-                  ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-                      fixedSize: Size.fromWidth(double.maxFinite),
-                      padding: EdgeInsets.symmetric(vertical: 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)
-                      ),
-                      backgroundColor: Colors.amber,
-                      foregroundColor: Colors.red[800]),
-        )
-      ),
-      
-      home: ProductListScreen()
-    );
-  }
 }
